c87cd529934772fc4e45e612703336bb883ee112
Heitor Palazzi Oliveira <88149578+HPalazzi@users.noreply.github.com>
2022-10-09 17:42:24 -0300

    Update index.py

--- python/minecraft em python/index.py
+++ python/minecraft em python/index.py
@@ -14,6 +14,12 @@ block_pick = 1
 
 def update():
     global  block_pick
+
+    if held_keys['right mouse'] or held_keys['left mouse']:
+        hand.active()
+    else:
+        hand.passive()
+
     if held_keys['1']: block_pick = 1
     if held_keys['2']: block_pick = 2
     if held_keys['3']: block_pick = 3
@@ -37,17 +43,44 @@ class Voxel(Button):# 1°
         if self.hovered:
             if key == 'right mouse down':
                 if block_pick == 1: voxel = Voxel(position= self.position + mouse.normal, texture= grass_texture)
-                if block_pick == 1: voxel = Voxel(position= self.position + mouse.normal, texture= stone_texture)
-                if block_pick == 1: voxel = Voxel(position= self.position + mouse.normal, texture= brick_texture)
-                if block_pick == 1: voxel = Voxel(position= self.position + mouse.normal, texture= dirt_texture)
+                if block_pick == 2: voxel = Voxel(position= self.position + mouse.normal, texture= stone_texture)
+                if block_pick == 3: voxel = Voxel(position= self.position + mouse.normal, texture= brick_texture)
+                if block_pick == 4: voxel = Voxel(position= self.position + mouse.normal, texture= dirt_texture)
         
             if key == 'left mouse down':
                 destroy(self)
 
+class Sky(Entity):
+    def __init__(self):
+        super().__init__(
+            paret = scene,
+            model = 'sphere',
+            texture = sky_texture,
+            double_sided = True,
+            scale = 150
+        )
+class Hand(Entity):
+    def __init__(self):
+        super().__init__(
+			parent = camera.ui,
+			model = 'assets/arm',
+			texture = arm_texture,
+			scale = 0.2,
+			rotation = Vec3(150,-10,0),
+			position = Vec2(0.4,-0.6))
+    def active(self):
+        self.position = Vec2(0.3,-0.5)    
+    def passive(self):
+        self.position = Vec2(0.4,-0.6)
+
 for z in range(20):
     for x in range(20):
         voxel = Voxel(position = (x,0,z))
+
 player = FPC()
+sky = Sky()
+hand = Hand()
+
 app.run()
 
 # Glossario
