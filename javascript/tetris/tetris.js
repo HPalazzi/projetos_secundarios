33969c02a41acb4691f49ae2159a6d82f947dab8
Heitor Palazzi Oliveira <88149578+HPalazzi@users.noreply.github.com>
2022-10-10 21:52:41 -0300

    fun

---
+++ javascript/tetris/tetris.js
@@ -0,0 +1,69 @@
+const imageSquareSize = 24
+const size = 40
+const fps =  24
+const gameSpeed = 5
+const canvas =  document.querySelector("#canvas")
+const image =  document.querySelector("#image")
+const ctx = canvas.getContext("2d")
+const squareCountX = canvas.width / size
+const squareCountY =  canvas.height /  size
+
+const shapes = [
+    new Tetris(0, 120, [
+      [0, 1, 0],
+      [0, 1, 0],
+      [1, 1, 0],
+    ]),
+    new Tetris(0, 96, [
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ]),
+    new Tetris(0, 72, [
+      [0, 1, 0],
+      [0, 1, 0],
+      [0, 1, 1],
+    ]),
+    new Tetris(0, 48, [
+      [0, 0, 0],
+      [0, 1, 1],
+      [1, 1, 0],
+    ]),
+    new Tetris(0, 24, [
+      [0, 0, 1, 0],
+      [0, 0, 1, 0],
+      [0, 0, 1, 0],
+      [0, 0, 1, 0],
+    ]),
+    new Tetris(0, 0, [
+      [1, 1],
+      [1, 1],
+    ]),
+  
+    new Tetris(0, 48, [
+      [0, 0, 0],
+      [1, 1, 0],
+      [0, 1, 1],
+    ]),
+  ];
+
+class  Tetris{
+    constructor(imageX, imageY, template){
+        this.imageX = imageX
+        this.imageY = imageY
+        this.template = template
+    }
+ }
+
+ checkBottom(){}
+ 
+ checkLeft(){}
+ 
+ checkRight(){}
+ 
+ moveLeft(){}
+ 
+ moveRight(){}
+ 
+ moveBottom(){}
+ changeRotation(){}
